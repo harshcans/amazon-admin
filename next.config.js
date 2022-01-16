c863f215f5fbbf4e6a23f4419912f4d01601ba59
harshcan <74178957+harshcan@users.noreply.github.com>
2022-01-16 20:07:26 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    BASE_URL: "https://zr7fi.sse.codesandbox.io",
+    MONGODB_URL:
+      " mongodb+srv://ecommerce:harshsingh@cluster0.vcotb.mongodb.net/ecommerce-nextjs?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "VY9D49zLjd6-7%G$e3h^7K=G*-9zuHFc$g@6L-Y?a_#hj2sE-M",
+    REFRESH_TOKEN_SECRET: "PNxtCN3!+VmrY#L_Wr2y2Y=rG2uJ-6FUZtDTqRPqY!FMbERkT3",
+    PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
+    CLOUD_UPDATE_PRESET: "YOUR_CLOUD_UPDATE_PRESET",
+    CLOUD_NAME: "YOUR_CLOUD_NAME",
+    CLOUD_API: "YOUR_CLOUD_API"
+  }
+};
